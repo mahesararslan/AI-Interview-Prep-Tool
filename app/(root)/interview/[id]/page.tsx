c3c47d8e9f1b24640edd8f976e275e91c5a24d10
mahesararslan <mahesararslan1998@gmail.com>
2025-03-27 02:07:18 +0500

    Creates feedback

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getInterviewById } from "@/lib/actions/general.action";
+import {getInterviewById} from "@/lib/actions/general.action";
 import {redirect} from "next/navigation";
 import Image from "next/image";
 import {getRandomInterviewCover} from "@/lib/utils";
@@ -29,8 +29,8 @@ const Page = async ({ params }: RouteParams) => {
             </div>
 
             <Agent
-                userName={user?.name}
-                type={user?.id}
+                userName={user?.name || ''}
+                userId={user?.id}
                 interviewId={id}
                 type="interview"
                 questions={interview.questions}
